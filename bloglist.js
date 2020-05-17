6bd972c57707d2896a6587030e078bcf63a0618a
nishchalkumar <43647131+nishchalkumar@users.noreply.github.com>
2020-05-17 20:28:40 +0530

    Add files via upload

---
+++ bloglist.js
@@ -0,0 +1,31 @@
+function fullPost(){
+    window.location.href="./post.html";
+  }
+  function onDeletePost(){
+    document.getElementById('deletePostPrompt').style.display='block';
+  }
+  
+  
+  function onCloseDeleteClicked() {
+    document.getElementById('deletePostPrompt').style.display='none';
+  }
+
+  function toogleSignUp() {
+    document.querySelector(".sign_up_modal").classList.toggle("open");
+}
+
+
+var overlay1= document.getElementById("modal");
+/*get the button that open the modal
+var open_model= document.getElementById("open_create");
+/*get the element that close model
+var close_btn=document. getElementsByClassName("close_create")[0];*/
+
+function toogleshowModal() {
+    document.querySelector(".overlay1").classList.toggle("open");
+  }
+
+  
+function toogleshowModalSign() {
+    document.querySelector(".sign_modal").classList.toggle("open");
+  }
---
+++ post.js
@@ -0,0 +1,21 @@
+function toogleSignUp() {
+    document.querySelector(".sign_up_modal").classList.toggle("open");
+}
+
+
+var overlay1= document.getElementById("modal");
+/*get the button that open the modal
+var open_model= document.getElementById("open_create");
+/*get the element that close model
+var close_btn=document. getElementsByClassName("close_create")[0];*/
+
+function toogleshowModal() {
+    document.querySelector(".overlay1").classList.toggle("open");
+  }
+
+  
+function toogleshowModalSign() {
+    document.querySelector(".sign_modal").classList.toggle("open");
+  }
+
+    
